6aeb05db3a24cc9fd36c86aa561eadde18de251e
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-31 15:53:40 +0530

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import UploadPage from './pages/UploadPage';
 import DashboardPage from './pages/DashboardPage';
 import SharedRoadmapPage from './pages/SharedRoadmapPage';
+import BackendStatus from './components/BackendStatus';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<UploadPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/shared" element={<SharedRoadmapPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<UploadPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/shared" element={<SharedRoadmapPage />} />
+      </Routes>
+      <BackendStatus />
+    </>
   );
 }
 
